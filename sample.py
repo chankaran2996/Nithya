ed664334b3ddb58ab1ad6b73cf8edc4680dde770
chankaran2996 <chandrasekaran2996@gmail.com>
2025-05-25 22:36:51 +0530

    25/5

--- sample.py
+++ sample.py
@@ -1,51 +1,62 @@
-print("Wellcome to Nithi's Friuths")
+print("WELCOME TO NITHI'S FRUIT SHOP")
 fruits=[]
 while 1:
     print("options")
-    print("1 - Add friuts")
-    print("2 - open shop")
-    print("3 - Exit program")
-    key=int(input("Enter the key"))
+    print("1-Add fruits")
+    print("2-open shop")
+    print("3- exit program")
+    key=int(input("enter the key"))
     if key==1:
-        fruit = input("Enter fruit name:")
-        fruitQunatity = int(input("Enter the fruit quanty in kg you brought"))
-        amtBuyed=int(input("Enter the amount in rupees"))
-        amtSell=int(input("Enter the amount in rupees at per kg of fruit"))
+        fruit=input("enter the fruit ")
+        fruit_quantity= int(input("enter the quantity in kg: "))
+        amount_buyed = int(input("enter the amount for fruits buyed"))
+        amount_sold = int(input("enter the amount of fruit per kg: "))
         obj={
             "fruit":fruit,
-            "fruitQuantity":fruitQunatity,
-            "amtBuyed":amtBuyed,
-            "amtSell":amtSell
+            "fruit_quantity": fruit_quantity,
+            "amount_buyed": amount_buyed,
+            "amount_sold": amount_sold
         }
         fruits.append(obj)
-        # print(fruits)
-    
-    if key ==2:
+        print(fruits)
+    if key==2:
         while 1:
-            print("Options")
-            print("1 - New buyer")
-            print("2 - Exchange")
-            print("3 - refund")
-            print("4 - shop close")
-            option = int(input("Enter the option"))
-            
-            if option == 1:
+            print("options")
+            print("1-new buyer")
+            print("2-exchange")
+            print("3-refund")
+            print("4-shop close")
+            options=int(input("enter the options"))
+            if options==1:
+                basket=[]
                 while 1:
-                    basket=[]
-                    print("PIN options")
-                    for i in range(0 , len(fruits)):
+                    print("PIN OPTIONS")
+                    for i in range(0,len(fruits)):
                         print(f"{i+1} - {fruits[i]["fruit"]}")
                     print("404 - Bill")
-                    pin=int(input("Enter the PIN:"))
+                    pin=int(input("enter the pin"))
                     if pin==404:
-                        break
+                        break 
                     else:
-                        weigth
-            
-            if option == 4:
+                        if (pin>len(fruits)):
+                            print("Enter the correct pin")
+                        else:
+                            weight = int(input("Enter weigth going to buy"))
+                            if(weight <= fruits[pin-1]["fruit_quantity"]):
+                                print("Fruit available ")
+                                fruits[pin-1]["fruit_quantity"]-= weight
+                                amount=weight*fruits[pin-1]["amount_sold"]
+                                item={
+                                    "fruit":fruits[pin-1]["fruit"],
+                                    "amount": amount,
+                                    "weight": weight
+                                }
+                                basket.append(item)
+                                print(basket)
+                            else:
+                                print("Sorry quantity is not avalible")
+                            
+            if options==4:
                 break
-    if key == 3:
+    if key==3:
         break
-    
-
-        
